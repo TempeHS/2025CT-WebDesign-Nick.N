567788dcc96e307cc98081d5ab5659860d5ce22b
NickWeiHaoNi <159206669+NickWeiHaoNi@users.noreply.github.com>
2024-03-11 14:13:55 +1100

    Start layout game

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt ("Enter your name");
+let playerGuess;
+let playerFeedback = "TEST";
+let computerGuess;
+loadDIV ();
 
-if (enterName = myIntro) {
-    alert(myIntro + 1);
+function randomNum() { }
+
+function loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 & 10"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+}
+
+function enterNumber () {
+    playerGuess = prompt ("Enter a number")
 }
