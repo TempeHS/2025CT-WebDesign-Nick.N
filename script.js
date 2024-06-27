27e3caa61a2a89d9ae042620ddbf1711645ab67b
NickWeiHaoNi <159206669+NickWeiHaoNi@users.noreply.github.com>
2024-06-27 10:31:32 +1000

    dark mode

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function DarkMode() {
+function darkMode() {
+   var element = document.getElementById("NavBarDiv");
+
+   element.classList.toggle("dark-mode");
+}
+function darkMode2() {
    var element = document.body;
+
    element.classList.toggle("dark-mode");
 }
